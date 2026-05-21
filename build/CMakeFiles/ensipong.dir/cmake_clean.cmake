16d520737ffd8c59cc8e5c4b2983b8c9b3e77d73
Teo <teo21maitr@gmail.com>
2026-05-21 08:30:43 +0200

    TOUT

---
+++ build/CMakeFiles/ensipong.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ensipong.dir/src/shmem_pong.c.o"
+  "CMakeFiles/ensipong.dir/src/shmem_pong.c.o.d"
+  "ensipong"
+  "ensipong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ensipong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/teomaitrot/Documents/Ensimag/6/système/tpchat/ensimag-shmem
+# Build directory: /Users/teomaitrot/Documents/Ensimag/6/système/tpchat/ensimag-shmem/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(pong "../tests/fourpong.pl")
+set_tests_properties(pong PROPERTIES  _BACKTRACE_TRIPLES "/Users/teomaitrot/Documents/Ensimag/6/système/tpchat/ensimag-shmem/CMakeLists.txt;51;add_test;/Users/teomaitrot/Documents/Ensimag/6/système/tpchat/ensimag-shmem/CMakeLists.txt;0;")
